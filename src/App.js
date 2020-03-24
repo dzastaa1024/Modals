43c81ee85bcd3ded15cfd82154469a6b5130dbf1
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-03-24 17:03:58 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import "./Components/Modal/modal";
+
+class App extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
