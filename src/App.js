f22aadf6cd987f233482e4ff31ab4f079075a24c
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-03-24 20:12:58 +0000

    create modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,117 @@
 import React, { Component } from "react";
 import "./App.css";
-import "./Components/Modal/modal";
+import styled from "styled-components";
+import Modal from "./Components/Modal/Modal";
 
 class App extends Component {
+  state = {
+    isModalSingup: false,
+    isModalUsers: false,
+    name: "",
+    surname: "",
+    users: []
+  };
+
+  openModal = key => {
+    this.setState({
+      [key]: true
+    });
+  };
+
+  closeModal = key => {
+    this.setState({
+      [key]: false
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: [e.target.value]
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const user = {
+      name: this.state.name,
+      surname: this.state.surname
+    };
+    this.setState({
+      users: this.state.users.concat(user)
+    });
+  };
+
+  renderModalSingupContent = () => (
+    <Form onSubmit={this.handleSubmit}>
+      <Name
+        value={this.state.name}
+        onChange={this.handleChange}
+        placeholder="Name"
+        name="name"
+      />
+      <Surname
+        value={this.state.surname}
+        onChange={this.handleChange}
+        placeholder="Surname"
+        name="surname"
+      />
+      <Submit>Submit</Submit>
+    </Form>
+  );
+
+  renderModalUsersContent = () => (
+    <Wrapper>
+      {this.state.users.map(user => (
+        <List>
+          <Item>{user.name}</Item>
+          <Item>{user.surname}</Item>
+        </List>
+      ))}
+    </Wrapper>
+  );
+
   render() {
-    return <div></div>;
+    console.log(this.state.users);
+    const { isModalSingup, isModalUsers } = this.state;
+    return (
+      <Wrapper>
+        <Button onClick={() => this.openModal("isModalSingup")}>Sign up</Button>
+        <Button onClick={() => this.openModal("isModalUsers")}>
+          Show me users
+        </Button>
+        {isModalSingup && (
+          <Modal
+            closeModal={this.closeModal}
+            id="isModalSingup"
+            modalContent={this.renderModalSingupContent()}
+          />
+        )}
+        {isModalUsers && (
+          <Modal
+            closeModal={this.closeModal}
+            id="isModalUsers"
+            modalContent={this.renderModalUsersContent()}
+          />
+        )}
+      </Wrapper>
+    );
   }
 }
 
 export default App;
+
+const Wrapper = styled.div``;
+
+const Button = styled.button``;
+
+const Form = styled.form``;
+
+const Name = styled.input``;
+
+const Surname = styled.input``;
+
+const Submit = styled.button``;
+
+const List = styled.li``;
+
+const Item = styled.ul``;
--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-const Modal = () => {
-  return <div></div>;
+const Modal = ({ closeModal, modalContent, id }) => {
+  return ReactDOM.createPortal(
+    <Dimmer onClick={() => closeModal(id)}>
+      <ModalBody onClick={e => e.stopPropagation()}>
+        <CloseIcon onClick={() => closeModal(id)}>X</CloseIcon>
+           <ModalContent>{modalContent}</ModalContent>
+      </ModalBody>
+    </Dimmer>,
+    document.getElementById("modal")
+  );
 };
 
 export default Modal;
+
+const Dimmer = styled.div``;
+
+const ModalBody = styled.div``;
+
+const CloseIcon = styled.span``;
+
+const ModalContent = styled.div``;
